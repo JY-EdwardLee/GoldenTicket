7c5f2bd2b5c27ec6fab1de7754494c02bd18e8f0
DESKTOP-HA97GMI\SSAFY <tpdnjs7878@naver.com>
2025-07-23 17:18:01 +0900

    feat : JWT 토큰 검사 및 인증 필터

--- Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/util/JwtAuthenticationFilter.java
+++ Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/util/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -18,6 +19,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
+    private final UserDetailsService customUserDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
