5c3ccac0412e8a623b738b07a89434dd215d70b1
KimDS <kimds5344@naver.com>
2025-07-28 14:41:14 +0900

    fix : 오류 수정

--- Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/service/PostServiceImpl.java
+++ Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/service/PostServiceImpl.java
@@ -32,201 +32,198 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
 
-  private final UserMapper userMapper;
-  private final PostMapper postMapper;
-  private final CommentMapper commentMapper;
+    private final UserMapper userMapper;
+    private final PostMapper postMapper;
+    private final CommentMapper commentMapper;
 
 
-  /**
-   * 게시글 작성
-   *
-   * @param email       작성자 정보
-   * @param postRequest 게시글 내용
-   * @return true
-   */
-  @Transactional
-  @Override
-  public boolean createPost(String email, PostRequest postRequest) {
-    try {
-      User user = userMapper.selectUserByEmail(email);
-      postRequest.setUserId(user.getUserId());
+    /**
+     * 게시글 작성
+     *
+     * @param email       작성자 정보
+     * @param postRequest 게시글 내용
+     * @return true
+     */
+    @Transactional
+    @Override
+    public boolean createPost(String email, PostRequest postRequest) {
+        try {
+            User user = userMapper.selectUserByEmail(email);
+            postRequest.setUserId(user.getUserId());
 
-      if (user.getIsBlock()) {
-        throw new BlockedUserException("차단된 사용자는 글을 작성할 수 없습니다.");
-      }
+            if (user.getIsBlock()) {
+                throw new BlockedUserException("차단된 사용자는 글을 작성할 수 없습니다.");
+            }
 
-      int result = postMapper.insertPost(postRequest);
+            int result = postMapper.insertPost(postRequest);
 
-      if (result != 1) {
-        throw new DatabaseOperationException("게시글 저장중 오류가 발생하였습니다.");
-      }
-    } catch (DataAccessException e) {
-      throw new DatabaseOperationException("게시글 작성에 실패하였습니다.");
-    } catch (BlockedUserException e) {
-      throw new BlockedUserException("차단된 사용자로 인해 작성에 실패하였습니다.");
-    } catch (Exception e) {
-      throw new PostCreateFailException("게시글 작성 중 오류가 발생하였습니다.");
-    }
-    return true;
-  }
-
-
-  /**
-   * 게시글 상세보기
-   *
-   * @param postId
-   * @return PostDetailResponse
-   */
-  @Transactional
-  @Override
-  public PostDetailResponse getPostDetail(Long postId) {
-    try {
-      // 게시물 정보
-      PostDetailResponse postDetailResponse = postMapper.selectPostById(postId);
-      if (postDetailResponse == null) {
-        throw new PostNotFoundException("해당 게시글이 존재하지 않습니다.");
-      }
-
-      // 조회수
-      postDetailResponse.setViewCount(postDetailResponse.getViewCount() + 1);
-      int result = postMapper.plusView(postId);
-      if (result == 0) {
-        throw new DatabaseOperationException("조회수 증가에 실패했습니다.");
-      }
-
-      // 댓글
-      List<CommentDetailResponse> comments = commentMapper.getComments(postId);
-      postDetailResponse.setCommentList(comments);
-
-      // 사용자 정보
-      Long userId = postMapper.selectUserIdByPostId(postId);
-
-      if (userId == null) {
-        throw new PostUserNotFoundException("게시글 작성자 정보가 존재하지 않습니다.");
-      }
-      PostUserResponse postUserResponse = userMapper.selectUserByPostId(userId);
-      if (postUserResponse == null) {
-        throw new PostUserNotFoundException("게시글 작성자 정보를 불러오는 데 실패했습니다.");
-      }
-      postDetailResponse.setPostUser(postUserResponse);
-
-      return postDetailResponse;
-
-    } catch (PostUserNotFoundException e) {
-      throw e;
-    } catch (DatabaseOperationException e) {
-      throw e;
-    } catch (PostNotFoundException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new PostRetrievalException("게시글 조회시 오류가 발생하였습니다.");
-    }
-  }
-
-  /**
-   * 게시글 수정
-   *
-   * @param email             수정을 시도하려는 사람의 이메일
-   * @param postUpdateRequest 수정 내용
-   * @return true
-   */
-  @Transactional
-  @Override
-  public boolean updatePost(String email, PostUpdateRequest postUpdateRequest) {
-    try {
-      User user = userMapper.selectUserByEmail(email);
-
-      if (user.getIsBlock()) {
-        throw new BlockedUserException("차단된 사용자는 글을 작성할 수 없습니다.");
-      }
-
-      if (!user.getUserRole().equals(UserRole.ADMIN)) {
-        if (user.getUserId() != postMapper.selectUserIdByPostId(
-            postUpdateRequest.getPostId())) {
-          throw new PostUpdateFailException("권한이 없습니다.");
+            if (result != 1) {
+                throw new DatabaseOperationException("게시글 저장중 오류가 발생하였습니다.");
+            }
+        } catch (DataAccessException e) {
+            throw new DatabaseOperationException("게시글 작성에 실패하였습니다.");
+        } catch (BlockedUserException e) {
+            throw new BlockedUserException("차단된 사용자로 인해 작성에 실패하였습니다.");
+        } catch (Exception e) {
+            throw new PostCreateFailException("게시글 작성 중 오류가 발생하였습니다.");
         }
-      }
-
-      int result = postMapper.updatePost(postUpdateRequest);
-
-      if (result != 1) {
-        throw new DatabaseOperationException("간단하게 작성하자!!!!");
-      }
-    } catch (BlockedUserException e) {
-      throw new BlockedUserException("차단된 사용자로 인해 작성에 실패하였습니다.");
-    } catch (PostUpdateFailException e) {
-      throw new PostUpdateFailException("게시글 접근 권한으로 인해 오류가 발생하였습니다");
-    } catch (Exception e) {
-      throw new PostUpdateException("게시글 수정시 오류가 발생하였습니다.");
+        return true;
     }
 
-    return true;
-  }
 
-  /**
-   * 게시글 삭제
-   *
-   * @param email  삭제를 시도하려는 사람의 이메일
-   * @param PostId 삭제하려는 게시글
-   * @return true
-   */
-  @Transactional
-  @Override
-  public boolean deletePost(String email, Long PostId) {
-    try {
-      User user = userMapper.selectUserByEmail(email);
+    /**
+     * 게시글 상세보기
+     *
+     * @param postId
+     * @return PostDetailResponse
+     */
+    @Transactional
+    @Override
+    public PostDetailResponse getPostDetail(Long postId) {
+        try {
+            // 게시물 정보
+            PostDetailResponse postDetailResponse = postMapper.selectPostById(postId);
+            if (postDetailResponse == null) {
+                throw new PostNotFoundException("해당 게시글이 존재하지 않습니다.");
+            }
 
-      if (!user.getUserRole().equals(UserRole.ADMIN)) {
-        if (user.getUserId() != postMapper.selectUserIdByPostId(PostId)) {
-          throw new PostDeleteFailException("권한이 없습니다.");
+            // 조회수
+            postDetailResponse.setViewCount(postDetailResponse.getViewCount() + 1);
+            int result = postMapper.plusView(postId);
+            if (result == 0) {
+                throw new DatabaseOperationException("조회수 증가에 실패했습니다.");
+            }
+
+            // 댓글
+            List<CommentDetailResponse> comments = commentMapper.getComments(postId);
+            postDetailResponse.setCommentList(comments);
+
+            // 사용자 정보
+            Long userId = postMapper.selectUserIdByPostId(postId);
+
+            if (userId == null) {
+                throw new PostUserNotFoundException("게시글 작성자 정보가 존재하지 않습니다.");
+            }
+            PostUserResponse postUserResponse = userMapper.selectUserByPostId(userId);
+            if (postUserResponse == null) {
+                throw new PostUserNotFoundException("게시글 작성자 정보를 불러오는 데 실패했습니다.");
+            }
+            postDetailResponse.setPostUser(postUserResponse);
+
+            return postDetailResponse;
+
+        } catch (PostUserNotFoundException | PostNotFoundException | DatabaseOperationException e) {
+            throw e;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new PostRetrievalException("게시글 조회시 오류가 발생하였습니다.");
         }
-      }
-
-      int result = postMapper.deletePost(PostId);
-
-      if (result != 1) {
-        throw new DatabaseOperationException("게시물 DB 삭제중 오류");
-      }
-    } catch (PostDeleteFailException e) {
-      throw new PostDeleteFailException("게시글 접근 권한으로 인해 오류가 발생하였습니다");
-    } catch (Exception e) {
-      e.printStackTrace();
-      throw new PostDeleteException("게시글 삭제시 오류가 발생하였습니다.");
     }
 
-    return true;
-  }
+    /**
+     * 게시글 수정
+     *
+     * @param email             수정을 시도하려는 사람의 이메일
+     * @param postUpdateRequest 수정 내용
+     * @return true
+     */
+    @Transactional
+    @Override
+    public boolean updatePost(String email, PostUpdateRequest postUpdateRequest) {
+        try {
+            User user = userMapper.selectUserByEmail(email);
 
-  /**
-   * 좋아요 누르기
-   *
-   * @param email  좋아요를 누른 사용자
-   * @param postId 게시글 번호
-   */
-  @Transactional
-  @Override
-  public PostLikeResponse likePost(String email, Long postId) {
+            if (user.getIsBlock()) {
+                throw new BlockedUserException("차단된 사용자는 글을 작성할 수 없습니다.");
+            }
 
-    try {
-      User user = userMapper.selectUserByEmail(email);
+            if (!user.getUserRole().equals(UserRole.ADMIN)) {
+                if (user.getUserId() != postMapper.selectUserIdByPostId(
+                    postUpdateRequest.getPostId())) {
+                    throw new PostUpdateFailException("권한이 없습니다.");
+                }
+            }
 
-      // 이미 좋아요를 눌렀다면 -> 좋아요 삭제
-      if (postMapper.selectLike(user.getUserId(), postId)) {
-        // 좋아요 삭제 메서드
-        postMapper.minusLike(postId);
-        postMapper.deletePostLike(user.getUserId(), postId);
+            int result = postMapper.updatePost(postUpdateRequest);
 
-      } else {
-        postMapper.plusLike(postId);
-        postMapper.insertPostLike(user.getUserId(), postId);
-      }
+            if (result != 1) {
+                throw new DatabaseOperationException("간단하게 작성하자!!!!");
+            }
+        } catch (BlockedUserException e) {
+            throw new BlockedUserException("차단된 사용자로 인해 작성에 실패하였습니다.");
+        } catch (PostUpdateFailException e) {
+            throw new PostUpdateFailException("게시글 접근 권한으로 인해 오류가 발생하였습니다");
+        } catch (Exception e) {
+            throw new PostUpdateException("게시글 수정시 오류가 발생하였습니다.");
+        }
 
-      // 좋아요 수 조회
-      Long likeCount = postMapper.selectPostLike(postId);
-
-      return new PostLikeResponse(postId, likeCount);
-    } catch (Exception e) {
-      throw new PostLikeFailException("게시글 좋아요 시 오류가 발생하였습니다.");
+        return true;
     }
-  }
+
+    /**
+     * 게시글 삭제
+     *
+     * @param email  삭제를 시도하려는 사람의 이메일
+     * @param PostId 삭제하려는 게시글
+     * @return true
+     */
+    @Transactional
+    @Override
+    public boolean deletePost(String email, Long PostId) {
+        try {
+            User user = userMapper.selectUserByEmail(email);
+
+            if (!user.getUserRole().equals(UserRole.ADMIN)) {
+                if (user.getUserId() != postMapper.selectUserIdByPostId(PostId)) {
+                    throw new PostDeleteFailException("권한이 없습니다.");
+                }
+            }
+
+            int result = postMapper.deletePost(PostId);
+
+            if (result != 1) {
+                throw new DatabaseOperationException("게시물 DB 삭제중 오류");
+            }
+        } catch (PostDeleteFailException e) {
+            throw new PostDeleteFailException("게시글 접근 권한으로 인해 오류가 발생하였습니다");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new PostDeleteException("게시글 삭제시 오류가 발생하였습니다.");
+        }
+
+        return true;
+    }
+
+    /**
+     * 좋아요 누르기
+     *
+     * @param email  좋아요를 누른 사용자
+     * @param postId 게시글 번호
+     */
+    @Transactional
+    @Override
+    public PostLikeResponse likePost(String email, Long postId) {
+
+        try {
+            User user = userMapper.selectUserByEmail(email);
+
+            // 이미 좋아요를 눌렀다면 -> 좋아요 삭제
+            if (postMapper.selectLike(user.getUserId(), postId)) {
+                // 좋아요 삭제 메서드
+                postMapper.minusLike(postId);
+                postMapper.deletePostLike(user.getUserId(), postId);
+
+            } else {
+                postMapper.plusLike(postId);
+                postMapper.insertPostLike(user.getUserId(), postId);
+            }
+
+            // 좋아요 수 조회
+            Long likeCount = postMapper.selectPostLike(postId);
+
+            return new PostLikeResponse(postId, likeCount);
+        } catch (Exception e) {
+            throw new PostLikeFailException("게시글 좋아요 시 오류가 발생하였습니다.");
+        }
+    }
 }
