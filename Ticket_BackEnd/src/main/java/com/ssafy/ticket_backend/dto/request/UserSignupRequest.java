364546d952e0188e3c66804165c4548d34149e2c
Sewon <tpdnjs7878@naver.com>
2025-07-29 10:10:37 +0900

    chore : 누락된 userId 추가

--- Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/dto/request/UserSignupRequest.java
+++ Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/dto/request/UserSignupRequest.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 // 유저 회원가입 요청 DTO
 public class UserSignupRequest {
 
+    private Long userId;
     private String email;
     private UserGender gender;
     private String userName;
