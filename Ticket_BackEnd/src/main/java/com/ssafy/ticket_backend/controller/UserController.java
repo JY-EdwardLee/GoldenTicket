46fefa5f4ed36a06d9a430708df94e2488fe9621
KimDS <kimds5344@naver.com>
2025-07-30 16:35:31 +0900

    fix : Test

--- Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/controller/UserController.java
+++ Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/controller/UserController.java
@@ -58,7 +58,7 @@ public class UserController {
     public RedirectView redirectToKakaoLogin() {
         String kakaoAuthUrl =
             "https://kauth.kakao.com/oauth/authorize" + "?client_id=" + KakaoRestApiKey
-                + "&redirect_uri=" + "http://localhost:8080" + "/users/auth/kakao/callback"
+                + "&redirect_uri=" + "http://i13a109.p.ssafy.io:8080" + "/users/auth/kakao/callback"
                 + "&response_type=code";
 
         return new RedirectView(kakaoAuthUrl);
