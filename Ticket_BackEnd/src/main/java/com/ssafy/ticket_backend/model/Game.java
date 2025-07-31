b8ca8dad7b5d359fd2995cbedcd1664bae5216cb
YongHoon <yy3082@naver.com>
2025-07-31 09:54:11 +0900

    경기일정 크롤링/s3

--- Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/model/Game.java
+++ Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/model/Game.java
@@ -2,14 +2,19 @@ package com.ssafy.ticket_backend.model;
 
 import java.time.LocalDateTime;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class Game {
 
-    Long gameId;
-    LocalDateTime gameDateTime;
-    BaseballTeams homeTeam;
-    BaseballTeams awayTeam;
-    boolean isCanceled;
-    boolean isEnded;
+  Long gameId;
+  LocalDateTime gameDateTime;
+  BaseballTeams homeTeam;
+  BaseballTeams awayTeam;
+  boolean isCanceled;
+  boolean isEnded;
+
+  // 잠시 test
+
 }
