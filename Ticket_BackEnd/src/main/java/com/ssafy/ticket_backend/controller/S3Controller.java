5d95161ab5b0bc61c5894ce73d284952a196621d
KimDS <kimds5344@naver.com>
2025-08-04 10:48:26 +0900

    refactor : 줄바꿈 수정

--- Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/controller/S3Controller.java
+++ Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/controller/S3Controller.java
@@ -26,9 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/s3")
 public class S3Controller {
 
-  private final S3UserService s3UserService;
-  private final S3PostService s3PostService;
-  private final UserMapper userMapper;
+    private final S3UserService s3UserService;
+    private final S3PostService s3PostService;
+    private final UserMapper userMapper;
 
   /*
   [정리]
@@ -50,105 +50,99 @@ public class S3Controller {
   타입과 id를 통해 key db에서 조회 -> 조회한걸로 url 발급 -> front에서 이미지 보기 끝.
    */
 
+    /**
+     * 이미지 업로드 수정을 위한 임시 Presigned URL요청
+     *
+     * @param type     UserProfile or PostImage
+     * @param refId    userId or postId
+     * @param fileName 원본파일 이름
+     * @return S3UploadUrlResponse
+     */
+    @GetMapping("/upload-url")
+    public ResponseEntity<S3UploadUrlResponse> getPresignedUploadUrl(
+        @AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam S3Type type,
+        @RequestParam Long refId, @RequestParam String fileName) {
 
-  /**
-   * 이미지 업로드 수정을 위한 임시 Presigned URL요청
-   *
-   * @param type     UserProfile or PostImage
-   * @param refId    userId or postId
-   * @param fileName 원본파일 이름
-   * @return S3UploadUrlResponse
-   */
-  @GetMapping("/upload-url")
-  public ResponseEntity<S3UploadUrlResponse> getPresignedUploadUrl(
-      @AuthenticationPrincipal CustomUserDetails userDetails,
-      @RequestParam S3Type type,
-      @RequestParam Long refId,
-      @RequestParam String fileName
-  ) {
+        S3UploadUrlResponse response;
 
-    S3UploadUrlResponse response;
+        // [마이페이지 CASE] => refId 값이 -1 이라면 마이페이지 -> 시큐리티에서 값 빼와
+        if (S3Type.UserProfile.equals(type) && refId == -1) {
+            String email = userDetails.getUsername();
+            User user = userMapper.selectUserByEmail(email);
+            refId = user.getUserId();
+        }
 
-    // [마이페이지 CASE] => refId 값이 -1 이라면 마이페이지 -> 시큐리티에서 값 빼와
-    if (S3Type.UserProfile.equals(type) && refId == -1) {
-      String email = userDetails.getUsername();
-      User user = userMapper.selectUserByEmail(email);
-      refId = user.getUserId();
+        if (S3Type.UserProfile.equals(type)) {
+            response = s3UserService.generatePresignedUploadUrl(refId, fileName);
+        } else if (S3Type.PostImage.equals(type)) {
+            response = s3PostService.generatePresignedUploadUrl(refId, fileName);
+        } else {
+            throw new IllegalArgumentException("지원하지 않는 이미지 타입입니다: " + type);
+        }
+
+        return ResponseEntity.ok(response);
     }
 
-    if (S3Type.UserProfile.equals(type)) {
-      response = s3UserService.generatePresignedUploadUrl(refId, fileName);
-    } else if (S3Type.PostImage.equals(type)) {
-      response = s3PostService.generatePresignedUploadUrl(refId, fileName);
-    } else {
-      throw new IllegalArgumentException("지원하지 않는 이미지 타입입니다: " + type);
+    /**
+     * Key값 데이터베이스 저장요청 front에서 S3 업로드 성공 후 요청
+     *
+     * @param s3SaveRequest 키값 업로드 정보
+     * @return S3SaveResponse
+     */
+    @PostMapping("/save-key")
+    public ResponseEntity<S3SaveResponse> saveUploadKey(
+        @AuthenticationPrincipal CustomUserDetails userDetails,
+        @RequestBody S3SaverRequest s3SaveRequest) {
+        S3SaveResponse response;
+
+        // [마이페이지 CASE] => refId 값이 -1 이라면 마이페이지 -> 시큐리티에서 값 빼와
+        if (S3Type.UserProfile.equals(s3SaveRequest.getType()) && s3SaveRequest.getRefId() == -1) {
+            String email = userDetails.getUsername();
+            User user = userMapper.selectUserByEmail(email);
+            s3SaveRequest.setRefId(user.getUserId());
+        }
+
+        if (S3Type.UserProfile.equals(s3SaveRequest.getType())) {
+            response = s3UserService.saveUploadKey(s3SaveRequest);
+        } else if (S3Type.PostImage.equals(s3SaveRequest.getType())) {
+            // 게시글 업데이트 시에만 사용한다.
+            response = s3PostService.saveUploadKey(s3SaveRequest);
+        } else {
+            throw new IllegalArgumentException("지원하지 않는 이미지 타입입니다: " + s3SaveRequest.getType());
+        }
+
+        return ResponseEntity.ok(response);
     }
 
-    return ResponseEntity.ok(response);
-  }
+    /**
+     * 조회 방식 [마이페이지] 요청 uerId -> -1로 고정 (시큐리티에서 값 뺴올 예정)
+     *
+     * @param s3DownloadRequest 키 조회정보
+     * @return S3DownloadResponse  응답 메세지(URL 포함)
+     */
+    @PostMapping("/download")
+    public ResponseEntity<S3DownloadResponse> getImageUrls(
+        @AuthenticationPrincipal CustomUserDetails userDetails,
+        @RequestBody S3DownloadRequest s3DownloadRequest) {
+        S3DownloadResponse s3DownloadResponse;
 
-  /**
-   * Key값 데이터베이스 저장요청 front에서 S3 업로드 성공 후 요청
-   *
-   * @param s3SaveRequest 키값 업로드 정보
-   * @return S3SaveResponse
-   */
-  @PostMapping("/save-key")
-  public ResponseEntity<S3SaveResponse> saveUploadKey(
-      @AuthenticationPrincipal CustomUserDetails userDetails,
-      @RequestBody S3SaverRequest s3SaveRequest) {
-    S3SaveResponse response;
+        // [마이페이지]
+        // s3DownloadRequest.getRefId() 값 -1이면 시큐리티에서 값 빼와.
+        if (S3Type.UserProfile.equals(s3DownloadRequest.getType())
+            && s3DownloadRequest.getRefId() == -1) {
+            String email = userDetails.getUsername();
+            User user = userMapper.selectUserByEmail(email);
+            s3DownloadRequest.setRefId(user.getUserId());
+        }
 
-    // [마이페이지 CASE] => refId 값이 -1 이라면 마이페이지 -> 시큐리티에서 값 빼와
-    if (S3Type.UserProfile.equals(s3SaveRequest.getType()) && s3SaveRequest.getRefId() == -1) {
-      String email = userDetails.getUsername();
-      User user = userMapper.selectUserByEmail(email);
-      s3SaveRequest.setRefId(user.getUserId());
+        if (S3Type.UserProfile.equals(s3DownloadRequest.getType())) {
+            s3DownloadResponse = s3UserService.getImageUrlsByTypeAndRefId(s3DownloadRequest);
+        } else if (S3Type.PostImage.equals(s3DownloadRequest.getType())) {
+            s3DownloadResponse = s3PostService.getImageUrlsByTypeAndRefId(s3DownloadRequest);
+        } else {
+            throw new IllegalArgumentException("지원하지 않는 이미지 타입입니다: " + s3DownloadRequest.getType());
+        }
+
+        return ResponseEntity.ok(s3DownloadResponse);
     }
-
-    if (S3Type.UserProfile.equals(s3SaveRequest.getType())) {
-      response = s3UserService.saveUploadKey(s3SaveRequest);
-    } else if (S3Type.PostImage.equals(s3SaveRequest.getType())) {
-      // 게시글 업데이트 시에만 사용한다.
-      response = s3PostService.saveUploadKey(s3SaveRequest);
-    } else {
-      throw new IllegalArgumentException("지원하지 않는 이미지 타입입니다: " + s3SaveRequest.getType());
-    }
-
-    return ResponseEntity.ok(response);
-  }
-
-  /**
-   * 조회 방식 [마이페이지] 요청 uerId -> -1로 고정 (시큐리티에서 값 뺴올 예정)
-   *
-   * @param s3DownloadRequest 키 조회정보
-   * @return S3DownloadResponse  응답 메세지(URL 포함)
-   */
-  @PostMapping("/download")
-  public ResponseEntity<S3DownloadResponse> getImageUrls(
-      @AuthenticationPrincipal CustomUserDetails userDetails,
-      @RequestBody S3DownloadRequest s3DownloadRequest
-  ) {
-    S3DownloadResponse s3DownloadResponse;
-
-    // [마이페이지]
-    // s3DownloadRequest.getRefId() 값 -1이면 시큐리티에서 값 빼와.
-    if (S3Type.UserProfile.equals(s3DownloadRequest.getType())
-        && s3DownloadRequest.getRefId() == -1) {
-      String email = userDetails.getUsername();
-      User user = userMapper.selectUserByEmail(email);
-      s3DownloadRequest.setRefId(user.getUserId());
-    }
-
-    if (S3Type.UserProfile.equals(s3DownloadRequest.getType())) {
-      s3DownloadResponse = s3UserService.getImageUrlsByTypeAndRefId(s3DownloadRequest);
-    } else if (S3Type.PostImage.equals(s3DownloadRequest.getType())) {
-      s3DownloadResponse = s3PostService.getImageUrlsByTypeAndRefId(s3DownloadRequest);
-    } else {
-      throw new IllegalArgumentException("지원하지 않는 이미지 타입입니다: " + s3DownloadRequest.getType());
-    }
-
-    return ResponseEntity.ok(s3DownloadResponse);
-  }
-
 }
