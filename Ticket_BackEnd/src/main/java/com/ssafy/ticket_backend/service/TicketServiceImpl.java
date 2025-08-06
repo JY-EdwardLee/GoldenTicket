f6560e218cccba0e65ed850ba6bf0fd268c058ed
KimDS <kimds5344@naver.com>
2025-08-06 15:25:55 +0900

    fix : SMS

--- Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/service/TicketServiceImpl.java
+++ Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/service/TicketServiceImpl.java
@@ -97,10 +97,12 @@ public class TicketServiceImpl implements TicketService {
 
                 User buyUser = userMapper.selectUserByUserId(buyer);
 
-                String text =
-                    "[골든티켓] 티켓 결제 안내" + "\n" + waitlist.getCreatedAt().getMonthValue() + "월 "
-                        + waitlist.getCreatedAt().getDayOfMonth() + "일 응모하신 티켓이 당첨되었습니다." + "\n"
-                        + "30분 이내 결제해주시기 바랍니다." + "\n";
+                String text = String.format("""
+                        [골든티켓] 티켓 결제 안내
+                        %d월 %d일 응모하신 티켓이 당첨되었습니다.
+                        30분 이내 결제해주시기 바랍니다.
+                        """, waitlist.getCreatedAt().getMonthValue(),
+                    waitlist.getCreatedAt().getDayOfMonth());
                 smsService.sendSMS(buyUser.getPhoneNumber(), text);
             } else {  // 대기열이 없다면
                 throw new TicketTransferException("응모자가 없습니다.");
