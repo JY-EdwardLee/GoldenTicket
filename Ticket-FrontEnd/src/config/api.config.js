29ea2812f4e84d2cf50254c58fc89ccfbd7b3594
edwardLee <ghfy1245.dev@gmail.com>
2025-08-08 15:17:34 +0900

    fix: 프론트 url 로호로 변경

--- Ticket-FrontEnd/src/config/api.config.js
+++ Ticket-FrontEnd/src/config/api.config.js
@@ -1,8 +1,8 @@
 // API 기본 URL 설정
 const API_BASE_URL =
   // 임시 수정
-  import.meta.env.VITE_API_BASE_URL || "https://www.xn--bb0b44m5yw9qd.xn--hu5b25b77nvwc.xn--3e0b707e";
-// import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+  // import.meta.env.VITE_API_BASE_URL || "https://www.xn--bb0b44m5yw9qd.xn--hu5b25b77nvwc.xn--3e0b707e";
+import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 import axios from "axios";
 axios.defaults.baseURL = API_BASE_URL;
