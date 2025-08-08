6c79d0156f81b51934af7ede6509862a00fb8c85
edwardLee <ghfy1245.dev@gmail.com>
2025-08-08 15:38:43 +0900

    fix: url 따옴표만

--- Ticket-FrontEnd/src/config/api.config.js
+++ Ticket-FrontEnd/src/config/api.config.js
@@ -1,9 +1,10 @@
 // API 기본 URL 설정
 const API_BASE_URL =
   // 임시 수정
-  import.meta.env.VITE_API_BASE_URL || "https://www.xn--bb0b44m5yw9qd.xn--hu5b25b77nvwc.xn--3e0b707e/api";
-// import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
-
+  // import.meta.env.VITE_API_BASE_URL || "https://www.xn--bb0b44m5yw9qd.xn--hu5b25b77nvwc.xn--3e0b707e/api";
+  // import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+  import.meta.env.VITE_API_BASE_URL || '';
+  
 import axios from "axios";
 axios.defaults.baseURL = API_BASE_URL;
 
