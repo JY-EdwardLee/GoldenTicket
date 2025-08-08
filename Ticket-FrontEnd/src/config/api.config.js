59c72a5a4b50f813e80cdc8388e326838ea37b70
edwardLee <ghfy1245.dev@gmail.com>
2025-08-08 15:49:49 +0900

    fix : /api만 남기기

--- Ticket-FrontEnd/src/config/api.config.js
+++ Ticket-FrontEnd/src/config/api.config.js
@@ -1,7 +1,7 @@
 // API 기본 URL 설정
 const API_BASE_URL =
   // 임시 수정
-  import.meta.env.VITE_API_BASE_URL || "https://www.xn--bb0b44m5yw9qd.xn--hu5b25b77nvwc.xn--3e0b707e/api";
+  import.meta.env.VITE_API_BASE_URL || "/api";
   // import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
   
 import axios from "axios";
