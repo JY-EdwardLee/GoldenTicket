55bc326602e09f555b14776bba84edd995aee108
KimDS <kimds5344@naver.com>
2025-08-12 14:11:32 +0900

    fix : 버그 픽스

--- Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/service/TicketServiceImpl.java
+++ Ticket_BackEnd/src/main/java/com/ssafy/ticket_backend/service/TicketServiceImpl.java
@@ -429,8 +429,8 @@ public class TicketServiceImpl implements TicketService {
         List<Ticket> tickets = ticketMapper.checkPayingOver30Minutes();
 
         for (Ticket ticket : tickets) {
-            Transaction transaction = transactionMapper.selectTransactionByTicketId(
-                ticket.getTicketId());
+            Transaction transaction = transactionMapper.selectTransactionByTicketIdAndUserId(
+                ticket.getTicketId(), ticket.getBuyerId());
 
             transaction.setTicketId(0L);
             transaction.setTransactionStatus(WaitlistStatus.CANCEL_WAITING.toString());
