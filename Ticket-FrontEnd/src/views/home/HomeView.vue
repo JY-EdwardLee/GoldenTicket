7a39dc9558537b4bfe2ac88d71c5eea9c78ef3c1
edwardLee <ghfy1245.dev@gmail.com>
2025-08-13 15:56:58 +0900

    test: 응답 url 확인

--- Ticket-FrontEnd/src/views/home/HomeView.vue
+++ Ticket-FrontEnd/src/views/home/HomeView.vue
@@ -593,7 +593,7 @@ const userRankingData = ref([]);
 const fetchUserRanking = async () => {
   try {
     const response = await axios.get(API_CONFIG.MAIN_PAGE.USER);
-    console.log(response.data);
+    console.log(response);
     // API 응답 데이터를 RankingCard 컴포넌트에 맞는 형태로 변환
     userRankingData.value = response.data.map(item => ({
       name: item.userName,
