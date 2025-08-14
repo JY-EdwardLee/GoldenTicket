024e32e97e11a32bc979f68fb5611e90474aa915
edwardLee <ghfy1245.dev@gmail.com>
2025-08-14 12:51:44 +0900

    apiconfig 수정

--- Ticket-FrontEnd/src/config/api.config.js
+++ Ticket-FrontEnd/src/config/api.config.js
@@ -28,12 +28,12 @@ const CHAT = {
 const AUTH = {
   KAKAO: `${API_BASE_URL}/users/auth/kakao`,
   NAVER: `${API_BASE_URL}/users/auth/naver`,
-  SIGNUP: `${API_BASE_URL}/users/signup`,
-  REFRESH: `${API_BASE_URL}/users/auth/refresh`,
-  SMS: `${API_BASE_URL}/users/signup/verification`,
-  SMS_VERIFY: `${API_BASE_URL}/users/signup/verification/check`,
-  LOGOUT: `${API_BASE_URL}/users/logout`,
-  TEMP_USER: `${API_BASE_URL}/users/auth/temp-user`,
+  SIGNUP: `/users/signup`,
+  REFRESH: `/users/auth/refresh`,
+  SMS: `/users/signup/verification`,
+  SMS_VERIFY: `/users/signup/verification/check`,
+  LOGOUT: `/users/logout`,
+  TEMP_USER: `/users/auth/temp-user`,
 };
 
 // 사용자 관련 엔드포인트
